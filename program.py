66c0523d1ef4680ae93ea60ec9ec8c11a15269ea
gobi_the_mansoe <courtney.pete@gmail.com>
2018-04-18 19:12:53 -0700

    added config.ini.example

---
+++ program.py
@@ -0,0 +1,33 @@
+import praw
+
+
+def main():
+    client_id = config['REDDITAPI']['client_id']
+    client_secret = config['REDDITAPI']['client_secret']
+    username = config['REDDITAPI']['username']
+    password = config['REDDITAPI']['password']
+    user_agent = config['REDDITAPI']['user_agent']
+    reddit = initialize_instance(client_id, client_secret, username, password, user_agent)
+    subreddit = reddit.subreddit('futurology')
+    hot_futurology = subreddit.hot(limit=5)
+    for post in hot_futurology:
+        if not post.stickied:
+            print(f'Title: {post.title}, Ups: {post.ups}, Downs: {post.downs}')
+
+
+
+def initialize_instance(client_id, client_secret, username, password, user_agent):
+    reddit = praw.Reddit(client_id=client_id,
+                         client_secret=client_secret,
+                         username=username,
+                         password=password,
+                         user_agent=user_agent)
+    return reddit
+
+
+if __name__ == '__main__':
+    import configparser
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    main()
