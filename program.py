138b2f7fb1fd174ed54a6436b395466d13256fde
gobi_the_mansoe <courtney.pete@gmail.com>
2018-04-18 21:28:22 -0700

    added config.ini.example

--- program.py
+++ program.py
@@ -1,16 +1,17 @@
 import praw
-
+from reddit_objects import Reddit
 
 def main():
-    client_id = config['REDDITAPI']['client_id']
-    client_secret = config['REDDITAPI']['client_secret']
-    username = config['REDDITAPI']['username']
-    password = config['REDDITAPI']['password']
-    user_agent = config['REDDITAPI']['user_agent']
-    reddit = initialize_instance(client_id, client_secret, username, password, user_agent)
-    subreddit = reddit.subreddit('futurology')
-    hot_futurology = subreddit.hot(limit=5)
-    for post in hot_futurology:
+    # client_id = config['REDDITAPI']['client_id']
+    # client_secret = config['REDDITAPI']['client_secret']
+    # username = config['REDDITAPI']['username']
+    # password = config['REDDITAPI']['password']
+    # user_agent = config['REDDITAPI']['user_agent']
+    # reddit = initialize_instance(client_id, client_secret, username, password, user_agent)
+    # subreddit = reddit.subreddit('futurology')
+    # hot_futurology = subreddit.hot(limit=5)
+    reddit = Reddit()
+    for post in reddit.get_hot_posts():
         if not post.stickied:
             print(f'Title: {post.title}, Ups: {post.ups}, Downs: {post.downs}')
 
---
+++ reddit_objects.py
@@ -0,0 +1,44 @@
+import praw
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+class Reddit(object):
+    """
+    Provides functionality to interact with praw which wraps the Reddit API
+    """
+    def __init__(self, subreddit='futurology'):
+        self.client_id = config['REDDITAPI']['client_id']
+        self.client_secret = config['REDDITAPI']['client_secret']
+        self.username = config['REDDITAPI']['username']
+        self.password = config['REDDITAPI']['password']
+        self.user_agent = config['REDDITAPI']['user_agent']
+        self.subreddit = subreddit
+
+        self.reddit = praw.Reddit(client_id=self.client_id,
+                                  client_secret=self.client_secret,
+                                  username=self.username,
+                                  password=self.password,
+                                  user_agent= self.user_agent)
+        self.subreddit = self.reddit.subreddit(subreddit)
+
+    def hot_posts(self, limit=5):
+        """
+        Top posts sorted by hot on the sub.
+        :param limit: number of posts to return defaults to 5
+        :return: generator containing post objects
+        """
+        return self.subreddit.hot(limit=limit)
+
+    def new_posts(self, limit=5):
+        """
+        Top posts sorted by new on the sub.
+        :param limit: number of posts to return defaults to 5
+        :return: generator containing post objects
+        """
+        return self.subreddit.new(limit=limit)
+
+    def get_user(self):
+        return self.reddit.user
